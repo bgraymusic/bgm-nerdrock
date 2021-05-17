3e88238b78dcc9b2bdd48788398b567dc98c81f7
bgraymusic <brian@briangraymusic.com>
2021-05-17 13:46:04 -0400

    Added web files from old site

---
+++ web/js/bandcamp.js
@@ -0,0 +1,24 @@
+// DISCOGRAPHY API
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+function Bandcamp() {
+	return this;
+}
+
+Bandcamp.apiURL = 'https://94iml3erc4.execute-api.us-east-1.amazonaws.com/dev/api/discography/{token}';
+Bandcamp.discography = {};
+
+Bandcamp.prototype = {
+	getBandcampData: function(cb, badges) {
+		Bandcamp.discography = {};
+		var oReq = new XMLHttpRequest();
+		oReq.addEventListener("load", function() {
+			Bandcamp.discography = JSON.parse(this.responseText).discography;
+			console.log('Discography fetched:');
+			console.log(JSON.parse(this.responseText));
+			cb(Bandcamp.discography);
+		});
+		oReq.open("GET", Bandcamp.apiURL.replace('{token}', badges.getToken()));
+		oReq.send();
+	}
+}
