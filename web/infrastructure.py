b3c5a6cfa9d6b5362ccf7ee84ab1622db6c66e13
Brian Gray <bgraymusic@gmail.com>
2024-12-30 16:48:18 -0500

    Need to have 1 instruction for a valid BPA resource (CDK bug)

--- web/infrastructure.py
+++ web/infrastructure.py
@@ -15,7 +15,8 @@ class WebConstruct(BgmConstruct):
         self.website_bucket = S3.Bucket(
             self, 'S3Bucket', bucket_name=context.physicalIdFor('web'),
             website_index_document='index.html', website_error_document='error.html',
-            block_public_access=S3.BlockPublicAccess(),  # counterintuitively allows public access
+            # counterintuitively allows public access
+            block_public_access=S3.BlockPublicAccess(ignore_public_acls=True),
             public_read_access=True, removal_policy=RemovalPolicy.DESTROY)
 
     def deployWebSite(self, distribution: CloudFront.IDistribution, context: BgmContext):
